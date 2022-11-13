66b3c4beb59c3ca6007748ebe2b47745c9bb553e
Marc Massot <marc.massot@hipra.com>
2022-11-13 19:55:25 +0100

    Move Method: Rental.getGameCharge to Game.getCharge

--- src/main/java/org/udg/caes/gamerent/Game.java
+++ src/main/java/org/udg/caes/gamerent/Game.java
@@ -12,4 +12,26 @@ public class Game {
 
     private final String title;
     private int priceCode;
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Game.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented -2)*1.5;
+                break;
+            case Game.NEW_RELEASE:
+                result += daysRented *3;
+                break;
+            case Game.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented -3)*1.5;
+                break;
+        }
+        return result;
+    }
+
+
 }
--- src/main/java/org/udg/caes/gamerent/Rental.java
+++ src/main/java/org/udg/caes/gamerent/Rental.java
@@ -11,27 +11,7 @@ public class Rental {
 
     double getCharge() {
         int daysRented = getDaysRented();
-        return getGameCharge(daysRented);
-    }
-
-    private double getGameCharge(int daysRented) {
-        double result = 0;
-        switch (getGame().getPriceCode()) {
-            case Game.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented -2)*1.5;
-                break;
-            case Game.NEW_RELEASE:
-                result += daysRented *3;
-                break;
-            case Game.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented -3)*1.5;
-                break;
-        }
-        return result;
+        return getGame().getCharge(daysRented);
     }
 
     int getFrequentRenterPoints() {
