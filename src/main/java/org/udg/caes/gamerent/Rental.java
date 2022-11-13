cdb86ef74000424bd18b21faa689c6dd1c4e6f27
Marc Massot <marc.massot@hipra.com>
2022-11-13 19:50:50 +0100

    Introduce variable: daysRented in Rental.getCharge

--- src/main/java/org/udg/caes/gamerent/Rental.java
+++ src/main/java/org/udg/caes/gamerent/Rental.java
@@ -11,19 +11,20 @@ public class Rental {
 
     double getCharge() {
         double result = 0;
+        int daysRented = getDaysRented();
         switch (getGame().getPriceCode()) {
             case Game.REGULAR:
                 result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented()-2)*1.5;
+                if (daysRented > 2)
+                    result += (daysRented -2)*1.5;
                 break;
             case Game.NEW_RELEASE:
-                result += getDaysRented()*3;
+                result += daysRented *3;
                 break;
             case Game.CHILDREN:
                 result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented()-3)*1.5;
+                if (daysRented > 3)
+                    result += (daysRented -3)*1.5;
                 break;
         }
         return result;
